2eb895a517a76bc881b42947250c9bebd3b9e4bb
StrikeZ <fpbrauner@gmail.com>
2023-02-27 11:11:18 +0100

    wolf orbit

--- Assets/6_Lesson_Zoo/Scripts/Animals/Wolf.cs
+++ Assets/6_Lesson_Zoo/Scripts/Animals/Wolf.cs
@@ -6,6 +6,28 @@ namespace Lesson_6.Animals
 {
     public class Wolf : HostileAnimal
     {
+        protected void Update()
+        {
+            if (CurrentState == AnimalState.IDLE && !busy)
+            {
+                Busy = true;
+                Vector3 nextGraze = Pen.transform.position;
+                Vector2 rand = Random.insideUnitCircle * 25f;
+                nextGraze.x += rand.x;
+                nextGraze.y = 2f;
+                nextGraze.z += rand.y;
+                StartCoroutine(LerpMovement(nextGraze)); ff
+            }
+            Vector3 orbitPosition = new Vector3(
+                _startPosition.x + (IdleArea * Mathf.Cos(_angle)),
+                _startPosition.y,
+                _startPosition.z + (IdleArea * Mathf.Sin(_angle))
+            );
+
+            transform.position = orbitPosition;
+
+            _angle += speed * Time.deltaTime;
+        }
 
     }
 }
