ae94018c7df22835ffa58047c34096f712d099a2
StrikeZ <fpbrauner@gmail.com>
2023-03-07 14:35:23 +0100

    movement piggy

--- Assets/6_Lesson_Zoo/Scripts/Animals/Pig.cs
+++ Assets/6_Lesson_Zoo/Scripts/Animals/Pig.cs
@@ -6,16 +6,16 @@ namespace Lesson_6.Animals
 {
     public class Pig : FriendlyAnimal
     {
-        protected void Update()
-        {
-    if (CurrentState == AnimalState.IDLE && !Busy)
+    protected void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(x, 0, z);
-        transform.Translate(movement * speed * Time.deltaTime);
-    }
+        if (CurrentState == AnimalState.IDLE && !Busy)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            transform.position += transform.forward * speed * Time.deltaTime;
+            Vector3 movement = new Vector3(x, 0, z);
+            transform.Translate(movement * speed * Time.deltaTime);
+        }
       
         }
 
